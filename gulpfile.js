339121114de8bd1192938306e5a6fa73313096c4
Ramiro G <mostlyphpsoftware@gmail.com>
2025-03-22 22:01:14 -0600

    comunicacion establecida entre sass y gulp atraves del package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
-export function hola(done) {
-    console.log("hola desde el gulpfile");
-    done();
+import { src , dest } from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
+
+export function css ( done ) {
+    src('src/scss/app.scss')
+    .pipe(sass())
+    .pipe(dest('build/css'));
+    done ();
 }
